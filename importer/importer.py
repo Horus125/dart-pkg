c9c37cd44e945608ddc2fa78320b60a214483285
Michael Jurka <mikejurka@google.com>
2017-03-07 21:25:37 -0800

    Add http package
    Change-Id: I88b88e8c32c462bef1115a8dc8b0978831e528e3

--- importer/importer.py
+++ importer/importer.py
@@ -26,7 +26,6 @@ IGNORED_EXTENSIONS = ['css', 'html', 'jpg', 'js', 'log', 'old', 'out', 'png', 'z
 LOCAL_PACKAGES = {
   'analyzer': '//dart/pkg/analyzer',
   'flutter': '//lib/flutter/packages/flutter',
-  'http': '//apps/modules/packages/flutter-http',
   'linter': '//dart/third_party/pkg/linter',
   'typed_mock': '//dart/pkg/typed_mock',
 }
