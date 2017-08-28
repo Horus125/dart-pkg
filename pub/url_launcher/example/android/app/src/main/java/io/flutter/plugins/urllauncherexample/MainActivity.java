2c95238fd160c52ff6b9e08ec3d4233a715eadb1
P.Y. Laligand <pylaligand@google.com>
2017-08-28 10:34:47 -0700

    Packages required for the new analysis method.
    TO-310
    
    Change-Id: Ifa9bb5a56c2046f37d507f76a29d6650a050d681

--- pub/url_launcher/example/android/app/src/main/java/io/flutter/plugins/urllauncherexample/MainActivity.java
+++ pub/url_launcher/example/android/app/src/main/java/io/flutter/plugins/urllauncherexample/MainActivity.java
@@ -1,4 +1,8 @@
-package io.flutter.plugins.url_launcher_example;
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.urllauncherexample;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
