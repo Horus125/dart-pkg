b86a2ef52e40dccc5ad4915b759bd3594a31d142
Vardhan Mudunuru <vardhan@google.com>
2017-11-20 13:53:04 -0800

    [dart] third party dart package: html_unescape
    Change-Id: I58145fa84d8a6eb75834ecbac1314398a6296e06

---
+++ html_unescape/CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+## 0.1.5
+
+- Remove deprecated dependency on `ChunkedConverter` 
+(author: [@andresaraujo](https://github.com/andresaraujo))
+- Loosen dev dependency on path
+
+## older
+
+(No records. Please see 
+[commit history](https://github.com/filiph/html_unescape/commits/master).)
---
+++ html_unescape/lib/html_unescape_small.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2016, Filip Hracek. All rights reserved. Use of this source
+// code is governed by a BSD-style license that can be found in the LICENSE
+// file.
+
+/// Unescapes HTML5-escaped strings. This version doesn't support all runes
+/// to decrease code size and increase performance.
+library html_unescape.small;
+
+import 'src/base.dart';
+import 'src/data/named_chars_basic.dart' as data;
+
+class HtmlUnescape extends HtmlUnescapeBase {
+  final int maxKeyLength = data.maxKeyLength;
+  final List<String> keys = data.keys;
+  final List<String> values = data.values;
+}
