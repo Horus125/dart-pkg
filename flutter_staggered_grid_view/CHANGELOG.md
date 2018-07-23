f2ecd5797c9fd54b499825c67992f6d197217a2c
Brijen Raval <braval@google.com>
2018-07-23 15:04:57 -0700

    [package][flutter] Add staggered grid view package
    Test: Build a mod in topaz with this package listed as dependency
    
    Change-Id: Ibc51e099d2f71ebaf70d067fcb2c00782e50108e

---
+++ flutter_staggered_grid_view/CHANGELOG.md
@@ -0,0 +1,15 @@
+## 0.1.4
+* Add `countBuilder` and `extendBuilder` constructors to `SliverStaggeredGrid`
+
+## 0.1.3
+* Remove Flutter SDK constraint
+
+## 0.1.2
+* Remove update Flutter SDK constraint
+
+## 0.1.1
+* Fix images in readme
+* Add dynamic resizing demo
+
+## 0.1.0
+* Initial Open Source release
---
+++ flutter_staggered_grid_view/lib/flutter_staggered_grid_view.dart
@@ -0,0 +1,6 @@
+library flutter_staggered_grid_view;
+
+export 'package:flutter_staggered_grid_view/src/staggered_tile.dart';
+export 'package:flutter_staggered_grid_view/src/sliver_staggered_grid.dart';
+export 'package:flutter_staggered_grid_view/src/staggered_grid_view.dart';
+export 'package:flutter_staggered_grid_view/src/sliver.dart';
